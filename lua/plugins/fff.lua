88c6b954abe9f7fa402038ec01b4a7e13cffb067
Leonardo Mariscal <l@mariscal.ch>
2025-09-09 00:23:42 +0100

    sync

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -1,6 +1,19 @@
 return {
     "dmtrKovalenko/fff.nvim",
     build = "cargo build --release",
-    opts = {},
+    dependencies = {
+        "folke/snacks.nvim"
+    },
+    opts = {
+        prompt = "> ",
+        title = "Find File",
+        layout = {
+            height = 0.7,
+            width = 0.7,
+            max_threads = 6,
+            preview_position = "left",
+            preview_size = 0.5
+        }
+    },
     lazy = false
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,7 +10,8 @@ return {
             {
                 ensure_installed = {
                     "lua", "c", "cpp", "vim", "python", "javascript", "scheme", "typescript", "wgsl", "json",
-                    "c_sharp", "rust", "slang", "nim", "proto", "janet_simple", "go", "pug" },
+                    "c_sharp", "rust", "slang", "nim", "proto", "janet_simple", "go", "pug", "html", "css", "typst",
+                },
                 highlight = { enable = true },
                 folding = { enable = true },
                 rainbow = { enable = true }
